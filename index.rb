44e06233bbe4bcfab555deb05f3c8f093b80baf7
Mohsen Azimi <msn.azi@gmail.com>
2015-01-27 18:30:33 -0800

    cleanup

--- index.rb
+++ index.rb
@@ -2,10 +2,8 @@ require 'maclight'
 require 'net/http'
 require 'json'
 
-
-# Turn both LEDs on
 token = '<<--YOUR TOKEN HERE-->'
-
 result = Net::HTTP.get(URI.parse("https://api.github.com/notifications?access_token=#{token}"))
 
+# Turn both LEDs on if there is Github notifications
 MacLight.capslock(JSON.parse(result).length > 0)
